eed91b5793b4c8783e826a4e87e64593f507d445
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-06-08 19:35:18 +0900

    refactor : remove dead code

--- SecretaryProblem_UnityEnv/Assets/Scripts/Secretary.cs
+++ SecretaryProblem_UnityEnv/Assets/Scripts/Secretary.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.AssetImporters;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Secretary : MonoBehaviour
 {
