df9f412dcf3bf9252b8a100113cc0a09a87a5ac0
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-06-12 14:14:52 +0900

    chore : env를 압축하여 upload

--- SecretaryProblem_PythonScripts/secretary_problem_case_mab_ucb.py
+++ SecretaryProblem_PythonScripts/secretary_problem_case_mab_ucb.py
@@ -1,55 +1,48 @@
 import mlagents
 import numpy as np
+np.bool = bool
 from mlagents_envs.environment import UnityEnvironment as UE                                    # 유니티 환경 Load
 from mlagents_envs.environment import ActionTuple 
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel  # 유니티 환경 엔진 설정 관리 클래스 (timescale 조절)
 # from torch.utils.tensorboard import SummaryWriter
-import random
 import math
-
-import numpy as np
-np.bool = bool
+import zipfile
+import os
 
 # Unity 환경 로드
+dir_path = "./SecretaryProblem_UnityEnv_Build/Env_MAB"
+file_name = "SecretaryProblem_UnityEnv.exe"
+
+if not os.path.isfile(os.path.join(dir_path, file_name)) :
+  with zipfile.ZipFile(dir_path + "/Env_MAB.zip", 'r') as zip_ref:
+    zip_ref.extractall(dir_path)
+
 engine_configuration_channel = EngineConfigurationChannel()   # 유니티 엔진의 timescale을 조절할 채널
-env = UE(file_name='./SecretaryProblem_UnityEnv_Build/Case_MAB/SecretaryProblem_UnityEnv', seed=1, side_channels=[engine_configuration_channel])
+env = UE(file_name=dir_path + "/" + file_name, seed=1, side_channels=[engine_configuration_channel])
 env.reset()
 
 # 유니티 브레인 설정 behavior (학습시킬 agent type) 불러오기
-behavior_name = list(env.behavior_specs.keys())[0] # "Case_1?team=0"
+behavior_name = list(env.behavior_specs.keys())[0]
 spec = env.behavior_specs[behavior_name]
 engine_configuration_channel.set_configuration_parameters(time_scale=10.0)  # 학습이 빨리 진행 될 수 있도록 10배속
 decision_steps, terminal_steps = env.get_steps(behavior_name)               # 각각 decision을 request한 step정보, terminate된 step 정보           
 
 # MAB를 위한 세팅
-
 def UCB(iters):
-  
   ucb = np.zeros(100)
   
-  #explore all the selections
   if iters < 100:
     return iters
-
   else:
     for k in range(100):
-      
-      # calculate upper bound
       upper_bound = math.sqrt((2*math.log(sum(count))) / count[k])
-
-      # add upper bound to the Q value
       ucb[k] = Q[k] + upper_bound
-      
-    # return the k which has maximum value
+
     return np.argmax(ucb)
   
-# number of rounds
-num_rounds = 20000
-# count of number of times an k was selected
+num_rounds = 10000
 count = np.zeros(100)
-# sum of rewards of each k
 sum_rewards = np.zeros(100)
-# Q value which is the average reward
 Q = np.zeros(100)
 
 for episode in range(num_rounds):
@@ -70,19 +63,14 @@ for episode in range(num_rounds):
     
     # 액션 결정
     k = UCB(episode)
-    
-    #action = spec.action_spec.random_action(len(decision_steps))
     action = ActionTuple(discrete=np.array([[k]], dtype=np.int32))
-        
-    # set actions
     env.set_actions(behavior_name, action)
     
     # 실제 액션 수행
     env.step()
     
     # 스탭 종료 후 에이전트의 정보 (보상, 상태) 확인
-    decision_steps, terminal_steps = env.get_steps(behavior_name)
-    
+    decision_steps, terminal_steps = env.get_steps(behavior_name)    
     count[k] += 1
      
     # state 상태별로 나누어 보상 저장
@@ -95,8 +83,11 @@ for episode in range(num_rounds):
     Q[k] = sum_rewards[k]/count[k]
       
   # 해당 에피소드에서의 보상 출력
-  print(f'total reward for ep {episode} is action : {k} and reward : {terminal_steps[tracked_agent].reward}')
+  print(f'episode {episode} is done, action : {k}, reward : {terminal_steps[tracked_agent].reward}')
 
 print('The optimal arm is {}'.format(np.argmax(Q)))
+for i in range(100) :
+  print(f'Q[{i}] : {Q[i]}')
+
 # 환경 종료
 env.close()
